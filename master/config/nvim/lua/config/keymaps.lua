5a38e647a2061d5fe86c3cfad17fd3fd8a098c3d
Toshihito Kon <toshihito.kon@gmail.com>
2026-03-22 22:58:42 +0900

    令和

--- master/config/nvim/lua/config/keymaps.lua
+++ master/config/nvim/lua/config/keymaps.lua
@@ -8,17 +8,28 @@ vim.keymap.set('n', ';', '/')
 vim.keymap.set('n', '<C-c><C-c>', ':noh<CR>', { silent = true })
 vim.keymap.set('i', '<C-c>', '<Esc>', { silent = true })
 
--- GitHub Copilot Chat
-vim.keymap.set('n', '<C-Bslash>', ':CopilotChatToggle<CR>', { silent = true })
+local function keymap_w_repeat(mode, sign, cmd)
+    vim.keymap.set(mode, sign,
+        function()
+            vim.cmd(cmd)
+            vim.cmd(string.format('silent! call repeat#set(\'%s\')', sign))
+        end,
+        { silent = true, desc = cmd}
+    )
+end
+
+-- Useful tool shortcuts. use 't' prefix
+-- Gitsigns
+vim.keymap.set('n', 'th', ':Gitsigns preview_hunk_inline<CR>', { silent = true })
+keymap_w_repeat('n', 'tj', 'Gitsigns nav_hunk next')
+
+-- Telescope
+vim.keymap.set('n', 'td', ':Telescope diagnostic<CR>', { silent = true })
+vim.keymap.set('n', 'tf', ':Telescope find_files<CR>', { silent = true })
+vim.keymap.set('n', 'ty', ':Telescope live_grep<CR>', { silent = true })
 
 -- LSP
 vim.keymap.set('n', '<Space>', ':lua vim.lsp.buf.hover()<CR>', { silent = true })
-
--- Buffers
-vim.keymap.set('n', '<C-b><C-b>', ':Telescope buffers<CR>', { silent = true })
-vim.keymap.set('n', '<C-b><C-q>', ':bdelete<CR>', { silent = true })
-vim.keymap.set('n', '<C-b><C-a>', ':%bd<CR>', { silent = true })
-vim.keymap.set('n', '<', ':b#<CR>', { silent = true })
 
 -- Terminal
 vim.keymap.set('t', '<C-p>', '<C-Bslash><C-N><C-c>', { silent = true })
